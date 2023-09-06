6231c5daa01ba9a1e495165f97ab658ac07d9e89
Charlie Andrews <charlie@asimov.com>
2023-09-06 11:04:53 -0400

    Basic repro for initialize command problems

---
+++ .devcontainer/initialize-command.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+echo "###############################################"
+echo "INITIALIZE COMMAND RUNNING"
+echo "###############################################"
+sleep 5
